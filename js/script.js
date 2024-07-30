99f388a6b4ba5e83ec81c50505882e91484fc223
Fariza Zea <farizazea9c@gmail.com>
2024-07-30 23:33:25 +0700

    yang sudah flex

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 // ini js
+alert("hi")
+
+document.getElementById('myForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Mencegah form dari pengiriman default
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  alert('Form submitted! Name: ' + name + ', Email: ' + email);
+});
