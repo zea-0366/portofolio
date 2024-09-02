0ae92ee8f9aa000a222689217b192f11f7e87b5f
Fariza Zea <farizazea9c@gmail.com>
2024-09-02 20:15:46 +0700

    gambar+isi about us

--- js/main.js
+++ js/main.js
@@ -27,10 +27,16 @@ const linkAction = () =>{
     navMenu.classList.remove('show-menu')
 }
  navLink.forEach(n => n.addEventListener('click', linkAction))
-// 13.00
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+const bgHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('bg-header') 
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', bgHeader)
+bgHeader()
 
 /*=============== SWIPER SERVICES ===============*/ 
 
